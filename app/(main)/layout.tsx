923584a77560bb4a8f126d0118eb4833ef57e258
Manoj-N00 <manojnaikj@gmail.com>
2025-07-02 20:52:06 +0530

    FIX:Added TRPC Backend

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,11 @@
 import { ReactNode } from "react";
-import { Vortex } from "@/components/aceternity-ui/vortex";
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-screen rounded-md  h-screen overflow-hidden">
-      {/* <Vortex
-        backgroundColor="black"
-        rangeY={800}
-        particleCount={100}
-        baseHue={120}
-        className="flex items-center flex-col justify-center w-full h-full"> */}
+    
         <div className="flex flex-col justify-start items-center rounded-md  pt-5 w-full h-full">
           {children}
         </div>
-      {/* </Vortex> */}
     </div>
   );
 };
